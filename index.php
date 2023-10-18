00b229804e0ad2ac8285ada682a071a1f7190379
SonechkacH <138005559+SonechkacH@users.noreply.github.com>
2023-10-18 13:25:22 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+<form method="POST" name="authorisation" action="http://dota2.com/game.php">
+   Логин: <input type="text" name="username"><br>
+   Пароль: <input type="password" name="user_password"><br>
+   <input type="submit" value="войти">
+</form>
+<!--http://dota2.com/game.php-->
